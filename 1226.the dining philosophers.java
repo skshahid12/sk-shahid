f1f44f4c45f0bd237a493b044586b2125bcc7fb2
skshahid12 <shaikshahid70327@gmail.com>
2026-07-23 14:25:12 +0530

    Implement Dining Philosophers problem solution

---
+++ 1226.the dining philosophers.java
@@ -0,0 +1,45 @@
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
+class DiningPhilosophers {
+
+    private final Semaphore semaphore = new Semaphore(4);
+    private final ReentrantLock[] forks = new ReentrantLock[5];
+
+    public DiningPhilosophers() {
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new ReentrantLock();
+        }
+    }
+
+    public void wantsToEat(
+            int philosopher,
+            Runnable pickLeftFork,
+            Runnable pickRightFork,
+            Runnable eat,
+            Runnable putLeftFork,
+            Runnable putRightFork) throws InterruptedException {
+
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+
+        semaphore.acquire();
+
+        forks[left].lock();
+        forks[right].lock();
+
+        try {
+            pickLeftFork.run();
+            pickRightFork.run();
+
+            eat.run();
+
+            putRightFork.run();
+            putLeftFork.run();
+        } finally {
+            forks[right].unlock();
+            forks[left].unlock();
+            semaphore.release();
+        }
+    }
+}
