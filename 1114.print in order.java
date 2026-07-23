6a055d3ffa4559dc11e6a2a16b431bacda22bbb2
skshahid12 <shaikshahid70327@gmail.com>
2026-07-23 14:39:16 +0530

    Create 1114.print in order.java

---
+++ 1114.print in order.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.Semaphore;
+
+class Foo {
+
+    private Semaphore second;
+    private Semaphore third;
+
+    public Foo() {
+        second = new Semaphore(0);
+        third = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first".
+        printFirst.run();
+
+        second.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        second.acquire();
+
+        // printSecond.run() outputs "second".
+        printSecond.run();
+
+        third.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        third.acquire();
+
+        // printThird.run() outputs "third".
+        printThird.run();
+    }
+}
