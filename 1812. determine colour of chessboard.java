84a22af2e522593282d9771573a40823c6895d7e
skshahid12 <shaikshahid70327@gmail.com>
2026-07-23 13:06:45 +0530

    Add Solution class to determine chessboard square color

---
+++ 1812. determine colour of chessboard.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        int col = coordinates.charAt(0) - 'a' + 1;
+        int row = coordinates.charAt(1) - '0';
+
+        return (col + row) % 2 != 0;
+    }
+}
