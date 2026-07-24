e32f6af22784f333b148e0a806cb94c01fd27d97
skshahid12 <shaikshahid70327@gmail.com>
2026-07-24 08:51:27 +0530

    Create 500.keyboard row.java

---
+++ 500.keyboard row.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            String w = word.toLowerCase();
+
+            String row;
+            if (row1.indexOf(w.charAt(0)) != -1) {
+                row = row1;
+            } else if (row2.indexOf(w.charAt(0)) != -1) {
+                row = row2;
+            } else {
+                row = row3;
+            }
+
+            boolean valid = true;
+            for (char c : w.toCharArray()) {
+                if (row.indexOf(c) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                result.add(word);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
