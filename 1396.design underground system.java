388bc2bf38ff377e3107e7d394365dcf04894831
skshahid12 <shaikshahid70327@gmail.com>
2026-07-24 11:41:00 +0530

    Create 1396.design underground system.java

---
+++ 1396.design underground system.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class UndergroundSystem {
+
+    // Stores active check-ins: id -> (station, time)
+    private Map<Integer, CheckIn> checkInMap;
+
+    // Stores route statistics: "start->end" -> (totalTime, tripCount)
+    private Map<String, Trip> tripMap;
+
+    public UndergroundSystem() {
+        checkInMap = new HashMap<>();
+        tripMap = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        checkInMap.put(id, new CheckIn(stationName, t));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        CheckIn checkIn = checkInMap.get(id);
+
+        String route = checkIn.station + "->" + stationName;
+        int travelTime = t - checkIn.time;
+
+        Trip trip = tripMap.getOrDefault(route, new Trip());
+        trip.totalTime += travelTime;
+        trip.count++;
+        tripMap.put(route, trip);
+
+        checkInMap.remove(id);
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        String route = startStation + "->" + endStation;
+        Trip trip = tripMap.get(route);
+        return (double) trip.totalTime / trip.count;
+    }
+
+    // Stores check-in information
+    class CheckIn {
+        String station;
+        int time;
+
+        CheckIn(String station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+    }
+
+    // Stores total travel time and trip count
+    class Trip {
+        int totalTime = 0;
+        int count = 0;
+    }
+}
